fbdc0da885c5de9975f258d438785aab83eb4c0b
stucky <6992917@qq.com>
2018-05-15 22:16:15 +0800

    Tuple

--- testContainer/main.cpp
+++ testContainer/main.cpp
@@ -37,13 +37,60 @@ void testMap()
 	{
 		std::cout << "Key:[" << value.first << "] Value:[" << value.second << "]\n";
 	}
-
 }
+
+auto getStudent(int id)
+{
+	if (id == 0)
+	{
+		return std::make_tuple(3.8, 'A', "zhangsan");
+	}
+	if (id == 1)
+	{
+		return std::make_tuple(2.9, 'C', "lisi");
+	}
+	if (id == 2)
+		return std::make_tuple(1.7, 'D', "wangwu");
+	return std::make_tuple(0.9, 'D', "NULL");
+}
+
+void testTuple()
+{
+	auto student = getStudent(0);
+	std::cout << "ID: 0, "
+		<< "GPA:" << std::get<0>(student) << ", "
+		<< "Grade:" << std::get<1>(student) << ", "
+		<< "Name:" << std::get<2>(student) << "\n";
+	double gpa;
+	char grade;
+	std::string name;
+	std::tie(gpa, grade, name) = getStudent(1);
+	std::cout << "ID: 1, "
+		<< "GPA:" << gpa << ", "
+		<< "Grade:" << grade << ", "
+		<< "Name:" << name << "\n";
+
+	auto new_tuple = std::tuple_cat(getStudent(0), getStudent(1));
+	double gpa1;
+	char grade1;
+	std::string name1;
+	std::tie(gpa, grade, name, gpa1, grade1, name1) = new_tuple;
+	std::cout << "ID: 0, "
+		<< "GPA:" << gpa << ", "
+		<< "Grade:" << grade << ", "
+		<< "Name:" << name << "\n";
+	std::cout << "ID: 1, "
+		<< "GPA:" << gpa1 << ", "
+		<< "Grade:" << grade1 << ", "
+		<< "Name:" << name1 << "\n";
+}
+
 
 int main()
 {
 	testArray();
 	testMap();
+	testTuple();
 	getchar();
 	return 0;
 }
