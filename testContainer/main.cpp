dc62cf3f2febb093f4c02806cdccf8a5abf8da73
stucky <6992917@qq.com>
2018-05-20 22:14:57 +0800

    ptr

--- testContainer/main.cpp
+++ testContainer/main.cpp
@@ -85,12 +85,77 @@ void testTuple()
 		<< "Name:" << name1 << "\n";
 }
 
+void testSharedPtr()
+{
+	auto pointer = std::make_shared<int>(10);
+	auto pointer2 = pointer;
+	auto pointer3 = pointer;
+	int *p = pointer.get();
+	std::cout << "pointer.use_count() = " << pointer.use_count() << std::endl;
+	std::cout << "pointer2.use_count() = " << pointer2.use_count() << std::endl;
+	std::cout << "pointer3.use_count() = " << pointer3.use_count() << std::endl;
+
+	pointer2.reset();
+	std::cout << "pointer.use_count() = " << pointer.use_count() << std::endl;
+	std::cout << "pointer2.use_count() = " << pointer2.use_count() << std::endl;
+	std::cout << "pointer3.use_count() = " << pointer3.use_count() << std::endl;
+
+	pointer3.reset();
+	std::cout << "pointer.use_count() = " << pointer.use_count() << std::endl;
+	std::cout << "pointer2.use_count() = " << pointer2.use_count() << std::endl;
+	std::cout << "pointer3.use_count() = " << pointer3.use_count() << std::endl;
+}
+
+struct Foo
+{
+	Foo()
+	{
+		std::cout << "Foo::Foo" << std::endl;
+	}
+	~Foo()
+	{
+		std::cout << "Foo::~Foo" << std::endl;
+	}
+	void foo()
+	{
+		std::cout << "Foo::foo" << std::endl;
+	}
+};
+
+void f(const Foo &)
+{
+	std::cout << "f(const Foo&)" << std::endl;
+}
+
+void testUniquePtr()
+{
+	std::unique_ptr<Foo> p1(std::make_unique<Foo>());
+	if (p1)
+		p1->foo();
+	{
+		std::unique_ptr<Foo> p2(std::move(p1));
+		f(*p2);
+		if (p2)
+			p2->foo();
+		if (p1)
+			p1->foo();
+		p1 = std::move(p2);
+		if (p2)
+			p2->foo();
+		std::cout << "~p2()" << std::endl;
+	}
+	if (p1)
+		p1->foo();
+}
+
 
 int main()
 {
 	testArray();
 	testMap();
 	testTuple();
+	testSharedPtr();
+	testUniquePtr();
 	getchar();
 	return 0;
 }
